18afc64ad5166d1600a4075ac6e9f04527822390
onecoffeeplz <110420215+onecoffeeplz@users.noreply.github.com>
2025-07-18 14:12:34 +0300

    add application icon

--- app/src/main/java/dev/onecoffeeplz/zamechalka/presentation/ui/components/ErrorView.kt
+++ app/src/main/java/dev/onecoffeeplz/zamechalka/presentation/ui/components/ErrorView.kt
@@ -2,7 +2,6 @@ package dev.onecoffeeplz.zamechalka.presentation.ui.components
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -10,7 +9,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import dev.onecoffeeplz.zamechalka.R
 
 @Composable
